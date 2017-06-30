645b23309919d0daf6198d7b8317b21a64e968fa
Benjamin Williams <benjaminwilliams@Benjamin-Williamss-MacBook-Pro.local>
2017-06-30 14:57:07 -0700

    add working index and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+
+50.times do |index|
+  Project.create!(name: Faker::Ancient.hero,
+                  price: Faker::Number.between(3, 374),
+                  origin: Faker::GameOfThrones.city)
+end
+
+p "Created #{Project.name} projects"
