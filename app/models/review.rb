21560610557b6e9854767c45e7637ab6bf148c10
Ben Williams <benjarwilliams@gmail.com>
2017-07-10 00:56:53 -0700

    add more validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :project
 
-  validates :author, :rating, :presence => true
+  validates :author, :presence => true
+  validates_numericality_of :rating, { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, :presence => true }
   validates :content_body, { length: { in: 50..250 }, :presence => true }
 
 
