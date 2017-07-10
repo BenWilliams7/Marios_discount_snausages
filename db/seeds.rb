e4b5cbab195864c191ac7a6843c612a244f2391c
Ben Williams <benjarwilliams@gmail.com>
2017-07-10 00:50:05 -0700

    add review seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 Project.destroy_all
+Review.destroy_all
 
 50.times do |index|
-  Project.create!(name: Faker::Ancient.hero,
+  project = Project.create!(name: Faker::Ancient.hero,
                   price: Faker::Number.between(3, 374),
                   origin: Faker::GameOfThrones.city)
+      5.times do |index|
+        project.reviews.create(author: Faker::Name.name,
+        content_body: Faker::Lorem.characters(100),
+        rating: Faker::Number.between(1, 5),
+        project_id: Faker::Number.between(1, 50))
+      end
 end
