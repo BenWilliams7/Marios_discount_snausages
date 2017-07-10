f2d0a1a50725f12ae6e250b5f09a38f10e5b5e5a
Ben Williams <benjarwilliams@gmail.com>
2017-07-10 02:06:05 -0700

    add full unit testing

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 describe Review do
   it { should validate_presence_of :author }
-  it { should belong_to :project }
+  it { should validate_presence_of :content_body }
+
+  it { should belong_to :product }
+
+  it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250) }
+
+  it { should validate_numericality_of(:rating)
+    .is_greater_than_or_equal_to(1)
+    .is_less_than_or_equal_to(5) }
+
 end
